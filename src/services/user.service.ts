d5d40f12a8083a6e2717230b07ec0341ca537a46
Adan Ghuman <adanghuman23@gmail.com>
2025-10-16 09:41:49 +0500

    Update user.service.ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,7 @@
 import { verifyUserRepository } from "../repositories/user.repository";
 
 export const verifyUserAndGetOrderService = async(visaNumber:string)=>{
+console.log("visa number....", visaNumber);
     const orderSummary = await verifyUserRepository(visaNumber);
     return orderSummary;
 }
